12273dee4032bec79e239a1f2900075a92a59475
ege türker <183089834+1etu@users.noreply.github.com>
2022-06-03 21:58:22 +0300

    Add build in the icon parameter sets

--- tools/resource-compiler/src/iconset.ts
+++ tools/resource-compiler/src/iconset.ts
@@ -21,3 +21,52 @@ export class PairError extends Error {
     this.detail = detail;
   }
 }
+
+export function build(arc: Qrc.Archive): Map<string, Set> {
+  const out = new Map<string, Set>();
+  let base: Set | undefined;
+
+  for (const file of arc.files.filter((f) => f.name.endsWith(MNU_EXT))) {
+    const text = Qrc.text(arc, file);
+    const id = idOf(file.name);
+
+    if (text.length === 0) {
+      out.set(id, { id, val: {}, ints: [] });
+      continue;
+    }
+
+    const val = Mnu.parse(text);
+    const kind = Mnu.kinds(text);
+
+    if (!(SIG_KEY in val)) {
+      throw new PairError(`${file.name} lacks ${SIG_KEY}`);
+    }
+
+    const set: Set = {
+      id,
+      val: Object.fromEntries(Object.entries(val).map(([k, v]) => [camel(k), v])),
+      ints: Object.keys(val)
+        .filter((k) => kind[k] === "int")
+        .map(camel)
+        .sort(),
+    };
+
+    if (id === BASE_ID) {
+      base = set;
+    }
+
+    out.set(id, set);
+  }
+
+  if (base === undefined) {
+    throw new PairError(`archive has no ${BASE_ID} icon set`);
+  }
+
+  for (const [id, set] of out) {
+    if (Object.keys(set.val).length === 0) {
+      out.set(id, { id, val: base.val, ints: base.ints });
+    }
+  }
+
+  return out;
+}
