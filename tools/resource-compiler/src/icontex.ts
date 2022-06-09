54c4fb9899bb5ad8caa579fd9867ae07aa3aa3af
ege türker <183089834+1etu@users.noreply.github.com>
2022-06-09 11:37:30 +0300

    Declare Tex in the icon texture set

--- tools/resource-compiler/src/icontex.ts
+++ tools/resource-compiler/src/icontex.ts
@@ -4,3 +4,8 @@ import * as Qrc from "./qrc.ts";
 const ATLAS_PX = 128;
 const ENV_PX = 64;
 const ENV_NAME = "dif";
+
+export interface Tex {
+  readonly name: string;
+  readonly img: Dds.Image;
+}
