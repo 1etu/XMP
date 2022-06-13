8819bda1b97aeb1bd25eed2240746e6ac10e6869
ege türker <183089834+1etu@users.noreply.github.com>
2022-06-13 14:18:30 +0300

    Add flip in the icon texture set

--- tools/resource-compiler/src/icontex.ts
+++ tools/resource-compiler/src/icontex.ts
@@ -19,3 +19,14 @@ export class SetError extends Error {
     this.detail = detail;
   }
 }
+
+function flip(img: Dds.Image): Dds.Image {
+  const stride = img.wid * 4;
+  const rgba = new Uint8Array(img.rgba.byteLength);
+
+  for (let y = 0; y < img.hgt; y += 1) {
+    rgba.set(img.rgba.subarray(y * stride, (y + 1) * stride), (img.hgt - 1 - y) * stride);
+  }
+
+  return { wid: img.wid, hgt: img.hgt, rgba };
+}
