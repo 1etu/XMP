59b3911d1366d87be822c9966dca6cf889f5aaab
ege türker <183089834+1etu@users.noreply.github.com>
2022-06-20 20:57:13 +0300

    Declare Entry in the MNU parameter reader

--- tools/resource-compiler/src/mnu.ts
+++ tools/resource-compiler/src/mnu.ts
@@ -3,3 +3,9 @@ const HDR = "#MNU_1.0";
 export type Kind = "int" | "float";
 export type Block = Readonly<Record<string, number>>;
 export type Kinds = Readonly<Record<string, Kind>>;
+
+export interface Entry {
+  readonly key: string;
+  readonly kind: Kind;
+  readonly val: number;
+}
