93291e92b66bfdacc2ad752e0058658e9defb6f2
ege türker <183089834+1etu@users.noreply.github.com>
2022-06-24 13:45:46 +0300

    Add entries in the MNU parameter reader

--- tools/resource-compiler/src/mnu.ts
+++ tools/resource-compiler/src/mnu.ts
@@ -39,3 +39,43 @@ function legacy(lines: readonly string[]): Entry[] {
 
   return out;
 }
+
+export function entries(src: string): Entry[] {
+  const lines = src.split(/\r?\n/);
+  const hdr = lines[0]?.trim();
+
+  if (hdr !== HDR) {
+    const old = legacy(lines);
+    if (old.length > 0) {
+      return old;
+    }
+    throw new FormatError(`want ${HDR}, got ${hdr ?? ""}`);
+  }
+
+  const out: Entry[] = [];
+
+  for (const line of lines.slice(1)) {
+    const s = line.trim();
+    if (s.length === 0) {
+      continue;
+    }
+
+    const a = s.indexOf(":");
+    const b = s.indexOf(":", a + 1);
+    if (a < 0 || b < 0) {
+      continue;
+    }
+
+    const key = s.slice(0, a).trim();
+    const kind = s.slice(a + 1, b).trim();
+    const val = Number(s.slice(b + 1).trim());
+
+    if (key.length === 0 || !Number.isFinite(val)) {
+      continue;
+    }
+
+    out.push({ key, kind: kind === "int" ? "int" : "float", val });
+  }
+
+  return out;
+}
