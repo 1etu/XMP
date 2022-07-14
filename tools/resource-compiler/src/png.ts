24069e027ec664ac4e142110446ee48c531e2214
ege türker <183089834+1etu@users.noreply.github.com>
2022-07-14 20:18:36 +0300

    Record CHAN in the PNG codec

--- tools/resource-compiler/src/png.ts
+++ tools/resource-compiler/src/png.ts
@@ -3,3 +3,4 @@ import { deflateSync, inflateSync } from "node:zlib";
 const SIG = Buffer.from([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]);
 const BIT_DEPTH = 8;
 const COLOR_RGBA = 6;
+const CHAN = 4;
