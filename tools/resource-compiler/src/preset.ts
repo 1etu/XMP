3f1bde8063a5bf3293979cf5cac52654e76da148
ege türker <183089834+1etu@users.noreply.github.com>
2022-08-05 13:10:25 +0300

    Record SIG in the wave presets

--- tools/resource-compiler/src/preset.ts
+++ tools/resource-compiler/src/preset.ts
@@ -4,3 +4,12 @@ import * as Qrc from "./qrc.ts";
 const N_CORNER = 4;
 const BASE_ID = "base";
 const CHAN = ["RED", "GREEN", "BLUE"] as const;
+
+const SIG: Readonly<Record<string, string>> = {
+  BACKGROUND: "1 RED",
+  HDR: "ENABLED",
+  LINE1: "STATE",
+  PARTICLES: "emit vel min",
+  PARTICLES_SPE: "delta time",
+  PARTICLES_UI: "brownian",
+};
