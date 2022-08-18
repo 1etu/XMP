4ab6c1e59dfa76913447fffa6d720a008714454c
ege türker <183089834+1etu@users.noreply.github.com>
2022-08-18 15:22:35 +0300

    Complete the wave presets with build

--- tools/resource-compiler/src/preset.ts
+++ tools/resource-compiler/src/preset.ts
@@ -97,3 +97,69 @@ function kindOf(name: string): string {
   const leaf = leafOf(name);
   return leaf.slice(0, leaf.lastIndexOf("."));
 }
+
+export function build(arc: Qrc.Archive): Map<string, Preset> {
+  const byId = new Map<string, Map<string, Src>>();
+
+  for (const file of arc.files.filter((f) => f.name.endsWith(".mnu"))) {
+    const name = file.name;
+    const text = Qrc.text(arc, file);
+
+    const src: Src =
+      text.length === 0
+        ? { val: {}, kind: {} }
+        : { val: Mnu.parse(text), kind: Mnu.kinds(text) };
+    const kind = kindOf(name);
+    const want = SIG[kind];
+    if (want !== undefined && Object.keys(src.val).length > 0 && !(want in src.val)) {
+      throw new PairError(`${name} lacks ${want}; table order broke`);
+    }
+
+    const id = idOf(name);
+    let group = byId.get(id);
+    if (group === undefined) {
+      group = new Map<string, Src>();
+      byId.set(id, group);
+    }
+    group.set(kind, src);
+  }
+
+  const base = byId.get(BASE_ID);
+  if (base === undefined) {
+    throw new PairError("archive has no base preset");
+  }
+
+  const out = new Map<string, Preset>();
+
+  const pick = (group: Map<string, Src>, kind: string): Src => {
+    const own = group.get(kind);
+    if (own !== undefined && Object.keys(own.val).length > 0) {
+      return own;
+    }
+    return base.get(kind) ?? { val: {}, kind: {} };
+  };
+
+  const group = (src: Src, skip: ReadonlySet<string>): Group => ({
+    val: rekey(src.val, skip),
+    ints: Object.keys(src.val)
+      .filter((k) => !skip.has(k) && src.kind[k] === "int")
+      .map(camel)
+      .sort(),
+  });
+
+  for (const [id, g] of byId) {
+    const bg = pick(g, "BACKGROUND");
+    const { rgb, keys } = corners(bg.val);
+
+    out.set(id, {
+      id,
+      corners: rgb,
+      bg: group(bg, keys),
+      hdr: group(pick(g, "HDR"), new Set()),
+      line: group(pick(g, "LINE1"), new Set()),
+      part: group(pick(g, "PARTICLES"), new Set()),
+    });
+  }
+
+  return out;
+}
