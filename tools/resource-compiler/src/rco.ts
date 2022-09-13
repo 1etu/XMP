b878234d0757396458e111b48945937f065f8c1a
ege türker <183089834+1etu@users.noreply.github.com>
2022-09-13 21:56:16 +0300

    Complete the RCO header reader with sounds

--- tools/resource-compiler/src/rco.ts
+++ tools/resource-compiler/src/rco.ts
@@ -18,3 +18,29 @@ export class FormatError extends Error {
     this.detail = detail;
   }
 }
+
+export function sounds(buf: Buffer): Sounds {
+  if (buf.byteLength < HDR_LEN) {
+    throw new FormatError(`short header: ${String(buf.byteLength)}`);
+  }
+
+  const magic = buf.readUInt32BE(0);
+  if (magic !== MAGIC) {
+    throw new FormatError(`want ${MAGIC.toString(16)}, got ${magic.toString(16)}`);
+  }
+
+  const off = buf.readUInt32BE(SND_OFF);
+  const len = buf.readUInt32BE(SND_SIZE);
+
+  if (off === NONE || len === NONE) {
+    return { off: 0, len: 0 };
+  }
+
+  if (off + len > buf.byteLength) {
+    throw new FormatError(
+      `sound ${String(off)}+${String(len)} past eof ${String(buf.byteLength)}`,
+    );
+  }
+
+  return { off, len };
+}
