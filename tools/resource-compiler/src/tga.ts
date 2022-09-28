133d7e999a2c94640025ea474bd56281d720db26
ege türker <183089834+1etu@users.noreply.github.com>
2022-09-28 16:49:29 +0300

    Add decode in the TGA lookup decoder

--- tools/resource-compiler/src/tga.ts
+++ tools/resource-compiler/src/tga.ts
@@ -26,3 +26,42 @@ export class FormatError extends Error {
     this.detail = detail;
   }
 }
+
+export function decode(buf: Buffer): Image {
+  const type = buf[TYPE_OFF] ?? 0;
+  if (type !== TYPE_RGB) {
+    throw new FormatError(`want type ${String(TYPE_RGB)}, got ${String(type)}`);
+  }
+  if ((buf[CMAP_OFF] ?? 0) !== 0) {
+    throw new FormatError("colour-mapped tga");
+  }
+
+  const wid = buf.readUInt16LE(WID_OFF);
+  const hgt = buf.readUInt16LE(HGT_OFF);
+  const bpp = buf[BPP_OFF] ?? 0;
+  const desc = buf[DESC_OFF] ?? 0;
+
+  if (bpp !== 24 && bpp !== 32) {
+    throw new FormatError(`want 24 or 32 bpp, got ${String(bpp)}`);
+  }
+
+  const step = bpp / 8;
+  const base = HDR_LEN + (buf[ID_OFF] ?? 0);
+  const rgba = new Uint8Array(wid * hgt * 4);
+
+  for (let y = 0; y < hgt; y += 1) {
+    const row = (desc & TOP_DOWN) !== 0 ? y : hgt - 1 - y;
+
+    for (let x = 0; x < wid; x += 1) {
+      const s = base + (y * wid + x) * step;
+      const d = (row * wid + x) * 4;
+
+      rgba[d] = buf[s + 2] ?? 0;
+      rgba[d + 1] = buf[s + 1] ?? 0;
+      rgba[d + 2] = buf[s] ?? 0;
+      rgba[d + 3] = step === 4 ? (buf[s + 3] ?? 0xff) : 0xff;
+    }
+  }
+
+  return { wid, hgt, bpp, rgba };
+}
