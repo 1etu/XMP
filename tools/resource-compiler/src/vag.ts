b16b8d7e54b9042223ff76d0d5a5e428ecf3b918
ege türker <183089834+1etu@users.noreply.github.com>
2022-11-14 16:09:50 +0300

    Add decode in the VAG audio decoder

--- tools/resource-compiler/src/vag.ts
+++ tools/resource-compiler/src/vag.ts
@@ -46,3 +46,50 @@ export class FormatError extends Error {
 function clamp(v: number): number {
   return v < PCM_MIN ? PCM_MIN : v > PCM_MAX ? PCM_MAX : v;
 }
+
+export function decode(buf: Buffer, off: number): Clip {
+  if (buf.subarray(off, off + 4).toString("latin1") !== MAGIC) {
+    throw new FormatError(`want ${MAGIC}`, off);
+  }
+
+  const len = buf.readUInt32BE(off + LEN_OFF);
+  const rate = buf.readUInt32BE(off + RATE_OFF);
+  const name = buf
+    .subarray(off + NAME_OFF, off + NAME_OFF + NAME_LEN)
+    .toString("latin1")
+    .replace(/\0.*$/, "");
+
+  const nFrame = Math.floor(len / FRAME_LEN);
+  const pcm = new Int16Array(nFrame * FRAME_PCM);
+
+  let h1 = 0;
+  let h2 = 0;
+  let w = 0;
+
+  for (let f = 0; f < nFrame; f += 1) {
+    const p = off + HDR_LEN + f * FRAME_LEN;
+    const head = buf[p] ?? 0;
+    const shift = head & 0x0f;
+    const pred = (head >> 4) & 0x0f;
+
+    if ((buf[p + 1] ?? 0) === FLAG_END) {
+      break;
+    }
+
+    const [c0, c1] = COEF[pred] ?? COEF[0] ?? [0, 0];
+
+    for (let i = 0; i < FRAME_PCM; i += 1) {
+      const byte = buf[p + 2 + (i >> 1)] ?? 0;
+      const nib = (i & 1) === 0 ? byte & 0x0f : byte >> 4;
+      const s = ((nib << NIB_SHIFT) << 16) >> 16;
+
+      const v = clamp((s >> shift) + ((c0 * h1 + c1 * h2) >> COEF_SHIFT));
+      pcm[w] = v;
+      w += 1;
+      h2 = h1;
+      h1 = v;
+    }
+  }
+
+  return { name, rate, pcm: pcm.subarray(0, w) };
+}
