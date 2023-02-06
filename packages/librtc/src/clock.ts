7407513c96b9e4632860c4fb080a0184ed44e5a4
ege türker <183089834+1etu@users.noreply.github.com>
2023-02-06 17:11:04 +0300

    Introduce Base in the system clock

--- packages/librtc/src/clock.ts
+++ packages/librtc/src/clock.ts
@@ -10,3 +10,33 @@ export interface Clock {
   advance(stampMs: number): Frame;
   reset(): void;
 }
+
+class Base implements Clock {
+  #no = 0;
+  #origin: number | undefined;
+  #prev = 0;
+
+  advance(stampMs: number): Frame {
+    if (this.#origin === undefined) {
+      this.#origin = stampMs;
+      this.#prev = stampMs;
+    }
+
+    const elapsedMs = stampMs - this.#origin;
+    const raw = stampMs - this.#prev;
+    this.#prev = stampMs;
+    this.#no += 1;
+
+    return {
+      no: this.#no,
+      deltaMs: Math.min(Math.max(raw, 0), MAX_DELTA_MS),
+      elapsedMs,
+    };
+  }
+
+  reset(): void {
+    this.#no = 0;
+    this.#origin = undefined;
+    this.#prev = 0;
+  }
+}
