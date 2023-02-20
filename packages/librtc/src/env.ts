7d03fbba91457ef6c1ce2434be519a7d56797ca1
ege türker <183089834+1etu@users.noreply.github.com>
2023-02-20 21:15:04 +0300

    Declare Sched in the daylight curve

--- packages/librtc/src/env.ts
+++ packages/librtc/src/env.ts
@@ -2,3 +2,10 @@ import type { Rtc } from "./rtc.js";
 import { hourOf, monthOf } from "./rtc.js";
 
 const N_MONTH = 12;
+
+export interface Sched {
+  readonly night2dayBegin: number;
+  readonly night2dayEnd: number;
+  readonly day2nightBegin: number;
+  readonly day2nightEnd: number;
+}
