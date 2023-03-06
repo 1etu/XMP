70c815866499a8b43613ce8e297731010768a4e5
ege türker <183089834+1etu@users.noreply.github.com>
2023-03-06 15:46:24 +0300

    Add daylightAt in the daylight curve

--- packages/librtc/src/env.ts
+++ packages/librtc/src/env.ts
@@ -25,3 +25,20 @@ function smoothstep(a: number, b: number, x: number): number {
 
   return t * t * (3 - 2 * t);
 }
+
+export function daylightAt(hour: number, s: Sched): number {
+  if (hour <= s.night2dayBegin) {
+    return 0;
+  }
+  if (hour < s.night2dayEnd) {
+    return smoothstep(s.night2dayBegin, s.night2dayEnd, hour);
+  }
+  if (hour <= s.day2nightBegin) {
+    return 1;
+  }
+  if (hour < s.day2nightEnd) {
+    return 1 - smoothstep(s.day2nightBegin, s.day2nightEnd, hour);
+  }
+
+  return 0;
+}
