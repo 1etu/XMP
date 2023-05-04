ae849eb08f0d0b7c0a7abefc62c490cbd6de2455
ege türker <183089834+1etu@users.noreply.github.com>
2023-05-04 15:22:31 +0300

    Record EVIDENCE in the classification scale

--- packages/resource/src/class.ts
+++ packages/resource/src/class.ts
@@ -1,3 +1,11 @@
 export const CLASSES = ["VERIFIED", "MEASURED", "DESIGN", "PROJECT", "INFERRED"] as const;
 
 export type Class = (typeof CLASSES)[number];
+
+const EVIDENCE: Readonly<Record<Class, number>> = {
+  VERIFIED: 0,
+  MEASURED: 1,
+  DESIGN: 2,
+  PROJECT: 3,
+  INFERRED: 4,
+};
