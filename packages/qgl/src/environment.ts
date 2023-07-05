2e3baecbdae6f3615100063f04a322494d8902b4
ege türker <183089834+1etu@users.noreply.github.com>
2023-07-05 17:29:50 +0300

    Complete the environment model with environmentAt

--- packages/qgl/src/environment.ts
+++ packages/qgl/src/environment.ts
@@ -14,3 +14,14 @@ const SCHEDULE: readonly (readonly [number, PresetId])[] = [
   [measured(23), "night"],
   [measured(24), "night"],
 ];
+
+export function environmentAt(cat: Catalog, hour: number): Preset {
+  const h = ((hour % 24) + 24) % 24;
+  for (let i = 1; i < SCHEDULE.length; i += 1) {
+    const a = SCHEDULE[i - 1];
+    const b = SCHEDULE[i];
+    if (a === undefined || b === undefined || h > b[0]) continue;
+    return lerp(cat[a[1]], cat[b[1]], (h - a[0]) / (b[0] - a[0]));
+  }
+  return cat.night;
+}
