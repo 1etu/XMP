9f7b9b3f0e495b0346d2a4163189f1a47cbec84d
ege türker <183089834+1etu@users.noreply.github.com>
2023-07-07 17:56:43 +0300

    Add the frame graph

---
+++ packages/qgl/src/frame.ts
@@ -0,0 +1,4 @@
+import type { Bg, Hdr, Line, Part, Rgb } from "./preset.js";
+import type { Lattice } from "./lines/spline.js";
+
+export const QUALITIES = ["static", "low", "standard", "high"] as const;
