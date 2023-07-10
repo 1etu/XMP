8e9eb7eb63140dfb21d82c7f23cf608e98f7f17a
ege türker <183089834+1etu@users.noreply.github.com>
2023-07-10 10:51:37 +0300

    Declare Quality in the frame graph

--- packages/qgl/src/frame.ts
+++ packages/qgl/src/frame.ts
@@ -2,3 +2,5 @@ import type { Bg, Hdr, Line, Part, Rgb } from "./preset.js";
 import type { Lattice } from "./lines/spline.js";
 
 export const QUALITIES = ["static", "low", "standard", "high"] as const;
+
+export type Quality = (typeof QUALITIES)[number];
