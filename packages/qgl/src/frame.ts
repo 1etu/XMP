5e4ce1382d9c0ad45889bb5e3f7b82b1d3b74064
ege türker <183089834+1etu@users.noreply.github.com>
2023-07-11 10:33:41 +0300

    Declare Scene in the frame graph

--- packages/qgl/src/frame.ts
+++ packages/qgl/src/frame.ts
@@ -10,3 +10,23 @@ export interface Viewport {
   readonly hgt: number;
   readonly dpr: number;
 }
+
+export interface Scene {
+  readonly corners: readonly Rgb[];
+  readonly bg: Bg;
+  readonly hdr: Hdr;
+  readonly line: Line;
+  readonly part: Part;
+  readonly daylight: number;
+  readonly palette: Uint8Array<ArrayBuffer>;
+  readonly linearPalette?: Float32Array<ArrayBuffer>;
+  readonly particleSoftness?: number;
+  readonly fresLut: Uint8Array<ArrayBuffer>;
+  readonly spline: Float32Array<ArrayBuffer>;
+  readonly normals: Float32Array<ArrayBuffer>;
+  readonly quality: Quality;
+  readonly reducedMotion: boolean;
+  readonly particleWarmup?: { readonly lattice: Lattice; readonly durationMs: number };
+  readonly waveTransfer?: readonly [number, number, number];
+  readonly waveGain?: number;
+}
