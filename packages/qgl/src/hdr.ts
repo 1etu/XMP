bef0a1c4aadde600661cbd4ef35fc3301804fb6d
ege türker <183089834+1etu@users.noreply.github.com>
2023-07-31 14:11:15 +0300

    Record MAX_GLARE_LEVELS in the HDR chain

--- packages/qgl/src/hdr.ts
+++ packages/qgl/src/hdr.ts
@@ -2,3 +2,4 @@ export const EXPOSURE_SAMPLES = verified(128);
 export const DISPLAY_GAIN = verified(0.8);
 export const GAUSSIAN_TAPS = verified(8);
 const MAX_GLARE_SIZE = design(9);
+const MAX_GLARE_LEVELS = design(9);
