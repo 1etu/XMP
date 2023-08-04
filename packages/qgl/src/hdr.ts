eddbeb26941789a95f634a926b0e27c75cc98a58
ege türker <183089834+1etu@users.noreply.github.com>
2023-08-04 13:51:28 +0300

    Implement halfTruncate in the HDR chain

--- packages/qgl/src/hdr.ts
+++ packages/qgl/src/hdr.ts
@@ -5,3 +5,13 @@ const MAX_GLARE_SIZE = design(9);
 const MAX_GLARE_LEVELS = design(9);
 const scratch = new Float32Array(1);
 const scratchBits = new Uint32Array(scratch.buffer);
+
+function halfTruncate(value: number): number {
+  if (value === 0 || !Number.isFinite(value)) return 0;
+  scratch[0] = value;
+  const bits = scratchBits[0] ?? 0;
+  const exponent = ((bits >>> 23) & 255) - 127;
+  if (exponent < -14) return Math.trunc(value * 16777216) / 16777216;
+  scratchBits[0] = bits & 0xffffe000;
+  return scratch[0];
+}
