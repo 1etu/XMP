ce7c90d35f9ddfc170667194ff296e9fb67cb3cc
ege türker <183089834+1etu@users.noreply.github.com>
2023-08-18 16:10:42 +0300

    Add the icon texture set

---
+++ packages/qgl/src/icontex.ts
@@ -0,0 +1,6 @@
+export * from "./icontex/const.js";
+export * from "./icontex/image.js";
+export * from "./icontex/kind.js";
+export * from "./icontex/load.js";
+export * from "./icontex/material.js";
+export * from "./icontex/shade.js";
