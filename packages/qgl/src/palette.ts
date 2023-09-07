e66bfd12d3d20291f9a0a63bf3ecad13688cc6e4
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-07 20:09:58 +0300

    Add blend in the month palette

--- packages/qgl/src/palette.ts
+++ packages/qgl/src/palette.ts
@@ -41,3 +41,33 @@ export function forMonth(file: PaletteFile, month: number): MonthPalette {
 
   return found;
 }
+
+export function blend(
+  pal: MonthPalette,
+  daylight: number,
+  out: Uint8Array<ArrayBuffer>,
+  next: MonthPalette = pal,
+  monthBlend = 0,
+): Uint8Array<ArrayBuffer> {
+  const t = Math.min(Math.max(daylight, 0), 1);
+  const m = Math.min(Math.max(monthBlend, 0), 1);
+
+  for (let i = 0; i < PAL_STOPS; i += 1) {
+    const night = pal.night[i] ?? [0, 0, 0];
+    const day = pal.day[i] ?? night;
+    const o = i * RGBA;
+
+    const nextNight = next.night[i] ?? night;
+    const nextDay = next.day[i] ?? day;
+    for (let k = 0; k < 3; k += 1) {
+      out[o + k] = mix(
+        mix(night[k] ?? 0, day[k] ?? 0, t),
+        mix(nextNight[k] ?? 0, nextDay[k] ?? 0, t),
+        m,
+      );
+    }
+    out[o + 3] = FULL;
+  }
+
+  return out;
+}
