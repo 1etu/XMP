de2987c88200050f03fb6be5cef3806e2aaa9737
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-13 17:45:23 +0300

    Add icon texture const

---
+++ packages/qgl/src/icontex/const.ts
@@ -0,0 +1,6 @@
+import { measured, verified } from "@vsh/resource";
+
+import type { Rgb } from "../preset.js";
+import type { Quad } from "./kind.js";
+
+export const LOG2_E = verified(1.4429434537887573);
