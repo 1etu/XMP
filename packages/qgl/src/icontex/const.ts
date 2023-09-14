3ccf931cfebb3fce572da4d219ce82b9b9843f37
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-14 14:22:10 +0300

    Record REFRACTION_FLOOR in icon texture const

--- packages/qgl/src/icontex/const.ts
+++ packages/qgl/src/icontex/const.ts
@@ -4,3 +4,4 @@ import type { Rgb } from "../preset.js";
 import type { Quad } from "./kind.js";
 
 export const LOG2_E = verified(1.4429434537887573);
+export const REFRACTION_FLOOR = verified(0.6669999957084656);
