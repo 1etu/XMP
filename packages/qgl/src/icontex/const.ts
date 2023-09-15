4ceed8d8f9bc3dc349c081f996504ad02b380b06
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-15 13:20:26 +0300

    Record LUMINANCE_POINTS in icon texture const

--- packages/qgl/src/icontex/const.ts
+++ packages/qgl/src/icontex/const.ts
@@ -5,3 +5,8 @@ import type { Quad } from "./kind.js";
 
 export const LOG2_E = verified(1.4429434537887573);
 export const REFRACTION_FLOOR = verified(0.6669999957084656);
+export const LUMINANCE_POINTS = [
+  [verified(0.2044299989938736), verified(-0.7366139888763428)],
+  [verified(0.5444409847259521), verified(-0.2599810063838959)],
+  [verified(1), verified(0)],
+] as const;
