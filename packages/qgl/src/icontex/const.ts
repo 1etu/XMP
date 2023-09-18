58899a3827b97f5657a14896f28605f0785bff0f
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-18 17:17:48 +0300

    Add DEFAULT_COLOR in icon texture const

--- packages/qgl/src/icontex/const.ts
+++ packages/qgl/src/icontex/const.ts
@@ -10,3 +10,8 @@ export const LUMINANCE_POINTS = [
   [verified(0.5444409847259521), verified(-0.2599810063838959)],
   [verified(1), verified(0)],
 ] as const;
+export const DEFAULT_COLOR: Rgb = [
+  measured(230 / 255),
+  measured(160 / 255),
+  measured(238 / 255),
+];
