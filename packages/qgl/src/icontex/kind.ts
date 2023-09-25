b69241eb2687201753ac6ad865d04c2bb90d2f40
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-25 15:52:25 +0300

    Declare Curve in icon texture kind

--- packages/qgl/src/icontex/kind.ts
+++ packages/qgl/src/icontex/kind.ts
@@ -2,3 +2,4 @@ import type { Rgb } from "../preset.js";
 
 export type Pair = readonly [number, number];
 export type Quad = readonly [number, number, number, number];
+export type Curve = readonly [Rgb, Rgb, Rgb];
