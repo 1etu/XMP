7526ad851ddac66c8e8a06fd816491895fc9da41
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-27 09:04:52 +0300

    Declare BackgroundSamples in icon texture kind

--- packages/qgl/src/icontex/kind.ts
+++ packages/qgl/src/icontex/kind.ts
@@ -3,3 +3,4 @@ import type { Rgb } from "../preset.js";
 export type Pair = readonly [number, number];
 export type Quad = readonly [number, number, number, number];
 export type Curve = readonly [Rgb, Rgb, Rgb];
+export type BackgroundSamples = readonly [IconTexture, IconTexture, number];
