4331bd6aa5f8d11dcd79016a3ed87db505e4d4da
ege türker <183089834+1etu@users.noreply.github.com>
2023-09-29 13:28:29 +0300

    Declare IconTexture in icon texture kind

--- packages/qgl/src/icontex/kind.ts
+++ packages/qgl/src/icontex/kind.ts
@@ -4,3 +4,9 @@ export type Pair = readonly [number, number];
 export type Quad = readonly [number, number, number, number];
 export type Curve = readonly [Rgb, Rgb, Rgb];
 export type BackgroundSamples = readonly [IconTexture, IconTexture, number];
+
+export interface IconTexture {
+  readonly data: Uint8ClampedArray | Float32Array;
+  readonly width: number;
+  readonly height: number;
+}
