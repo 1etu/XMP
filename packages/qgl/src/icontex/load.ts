cbd69a2167f290c250e5c5fc85305ddfd749d60f
ege türker <183089834+1etu@users.noreply.github.com>
2023-10-09 20:58:08 +0300

    Implement shadeLoadedIcons in icon texture load

--- packages/qgl/src/icontex/load.ts
+++ packages/qgl/src/icontex/load.ts
@@ -25,3 +25,28 @@ export async function loadIconTextures(
   for (const [id, normal] of entries) if (normal !== undefined) normals[id] = normal;
   return { normals, diffuse, environment };
 }
+
+export function shadeLoadedIcons(
+  textures: IconTextures,
+  material: IconMaterial | ((id: number) => IconMaterial) = iconMaterialOf(),
+): Readonly<Record<number, string>> {
+  const entries = Object.entries(textures.normals).map(([key, normal]) => {
+    const id = Number(key);
+    const canvas = document.createElement("canvas");
+    canvas.width = normal.width;
+    canvas.height = normal.height;
+    const ctx = canvas.getContext("2d");
+    if (ctx === null) return [id, ""] as const;
+    const image = ctx.createImageData(normal.width, normal.height);
+    image.data.set(normal.data);
+    shadeIcon(
+      image,
+      textures.diffuse,
+      textures.environment,
+      typeof material === "function" ? material(id) : material,
+    );
+    ctx.putImageData(image, 0, 0);
+    return [id, canvas.toDataURL("image/png")] as const;
+  });
+  return Object.fromEntries(entries);
+}
