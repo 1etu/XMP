6bf47e9901d96f25a29501dc3e0096592ba45cef
ege türker <183089834+1etu@users.noreply.github.com>
2023-10-13 10:30:12 +0300

    Implement luminanceCurve in icon texture material

--- packages/qgl/src/icontex/material.ts
+++ packages/qgl/src/icontex/material.ts
@@ -14,3 +14,13 @@ function light(x: number, z: number): Rgb {
   const b = (z * Math.PI) / 180;
   return [Math.sin(b) * Math.cos(a), Math.sin(b) * Math.sin(a), Math.cos(b)];
 }
+
+function luminanceCurve(glass: number): Curve {
+  const coefficient = (i: 0 | 1 | 2, j: 0 | 1 | 2, k: 0 | 1 | 2): Rgb => {
+    const [x, y] = LUMINANCE_POINTS[i];
+    const a = LUMINANCE_POINTS[j][0];
+    const b = LUMINANCE_POINTS[k][0];
+    return [a, b, (y * glass) / ((x - a) * (x - b))];
+  };
+  return [coefficient(0, 1, 2), coefficient(1, 0, 2), coefficient(2, 0, 1)];
+}
