5f77bd1a016768a5c64fbb6d915ac84759f3616b
ege türker <183089834+1etu@users.noreply.github.com>
2023-10-20 09:24:56 +0300

    Implement backgroundSamples in icon texture shade

--- packages/qgl/src/icontex/shade.ts
+++ packages/qgl/src/icontex/shade.ts
@@ -20,3 +20,29 @@ function sample(image: IconTexture, u: number, v: number, channel: number): numb
   const value = a + (b - a) * tx + (c + (d - c) * tx - a - (b - a) * tx) * ty;
   return image.data instanceof Float32Array ? value : value / 255;
 }
+
+function backgroundSamples(material: IconMaterial): BackgroundSamples | undefined {
+  if (material.background === undefined) return undefined;
+  let lower = material.background;
+  let upper = lower;
+  const maxLevel = Math.floor(Math.log2(Math.max(lower.width, lower.height)));
+  const lod = Math.max(0, Math.min(maxLevel, material.backgroundLod));
+  for (let level = 1; level <= Math.ceil(lod); level += 1) {
+    const width = Math.max(1, Math.floor(upper.width / 2));
+    const height = Math.max(1, Math.floor(upper.height / 2));
+    const data = new Float32Array(width * height * 4);
+    for (let y = 0; y < height; y += 1)
+      for (let x = 0; x < width; x += 1)
+        for (let channel = 0; channel < 4; channel += 1)
+          data[(y * width + x) * 4 + channel] = sample(
+            upper,
+            (x + 0.5) / width,
+            (y + 0.5) / height,
+            channel,
+          );
+    lower = upper;
+    upper = { data, width, height };
+  }
+  if (lod === Math.floor(lod)) lower = upper;
+  return [lower, upper, lod - Math.floor(lod)];
+}
