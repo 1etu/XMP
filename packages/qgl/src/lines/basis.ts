2c31803e03e22c9db416c5d9e408a670a9f27ce5
ege türker <183089834+1etu@users.noreply.github.com>
2023-10-30 20:09:59 +0300

    Implement derivative in the B-spline basis

--- packages/qgl/src/lines/basis.ts
+++ packages/qgl/src/lines/basis.ts
@@ -6,3 +6,11 @@ export function basis(t: number, out: Float32Array, offset = 0): void {
   out[offset + 2] = (1 + 3 * t + 3 * t2 - 3 * t3) / 6;
   out[offset + 3] = t3 / 6;
 }
+
+export function derivative(t: number, out: Float32Array, offset = 0): void {
+  const t2 = t * t;
+  out[offset] = (-(1 - t) * (1 - t)) / 2;
+  out[offset + 1] = 1.5 * t2 - 2 * t;
+  out[offset + 2] = 0.5 + t - 1.5 * t2;
+  out[offset + 3] = t2 / 2;
+}
