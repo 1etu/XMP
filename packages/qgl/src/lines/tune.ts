7d1bb63580c6cde0f86791fa952d4c9c7a957832
ege türker <183089834+1etu@users.noreply.github.com>
2023-11-06 16:22:35 +0300

    Record CONTROL_SIZE in the tuning constants

--- packages/qgl/src/lines/tune.ts
+++ packages/qgl/src/lines/tune.ts
@@ -2,3 +2,6 @@ import { design, measured, verified } from "@vsh/resource";
 
 export const STEX_W = measured(128);
 export const STEX_H = measured(128);
+export const CONTROL_SIZE = measured(19);
+
+export const COMPONENTS = 4;
