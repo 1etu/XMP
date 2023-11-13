5eef44f6211e628acc8b053842b8ee894d3537ed
ege türker <183089834+1etu@users.noreply.github.com>
2023-11-13 20:11:50 +0300

    Record NEAR in the tuning constants

--- packages/qgl/src/lines/tune.ts
+++ packages/qgl/src/lines/tune.ts
@@ -13,3 +13,5 @@ export const FFD_TIME_SCALE = verified(10);
 export const MAX_FRAME_GAP = design(1000);
 export const FOVY = measured(71.846);
 export const ASPECT = measured(16 / 9);
+export const NEAR = measured(0.1);
+export const FAR = measured(1000);
