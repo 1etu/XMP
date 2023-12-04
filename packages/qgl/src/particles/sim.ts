fb07aeea493226bcc54aa868dc043cecd6dd5a40
ege türker <183089834+1etu@users.noreply.github.com>
2023-12-04 16:48:47 +0300

    Record MAX_FRAME_GAP in the particle simulation

--- packages/qgl/src/particles/sim.ts
+++ packages/qgl/src/particles/sim.ts
@@ -9,3 +9,5 @@ export const CAMERA_Z = verified(2);
 export const LIFE_BOUND = verified(10);
 const ASPECT = measured(16 / 9);
 const FIXED_HZ = verified(60);
+const MAX_FRAME_GAP = design(1000);
+const DEAD_AGE = verified(-666);
