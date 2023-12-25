f1004eaf98f7b1b71062c04da1601215bff1fa1d
ege türker <183089834+1etu@users.noreply.github.com>
2023-12-25 15:14:18 +0300

    Record LIFE_FADE_IN in the particle view

--- packages/qgl/src/particles/view.ts
+++ packages/qgl/src/particles/view.ts
@@ -13,3 +13,4 @@ export { MAX_PARTICLES } from "./sim.js";
 export const PARTICLE_FLOATS = 16;
 const ASPECT = measured(16 / 9);
 const DISPLAY_GAIN = measured(0.8);
+const LIFE_FADE_IN = verified(0.02);
