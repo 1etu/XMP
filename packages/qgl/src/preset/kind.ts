58683fa11dd4fb3918c650edfdc64c4f85e805d7
ege türker <183089834+1etu@users.noreply.github.com>
2024-01-16 20:34:16 +0300

    Declare Group in preset kind

--- packages/qgl/src/preset/kind.ts
+++ packages/qgl/src/preset/kind.ts
@@ -2,3 +2,8 @@ import type { PresetId } from "./id.js";
 
 export type Rgb = readonly [number, number, number];
 export type Par = Readonly<Record<string, number>>;
+
+export interface Group {
+  readonly val: Par;
+  readonly ints: readonly string[];
+}
