e2aee089371677bd0056d2ec8d66d27fe5882bb2
ege türker <183089834+1etu@users.noreply.github.com>
2024-01-23 21:54:58 +0300

    Implement mixPar in preset mix

--- packages/qgl/src/preset/mix.ts
+++ packages/qgl/src/preset/mix.ts
@@ -5,3 +5,25 @@ const N_CORNER = 4;
 export function mix(a: number, b: number, t: number): number {
   return a + (b - a) * t;
 }
+
+function mixPar(a: Group, b: Group, t: number): Group {
+  const out: Record<string, number> = {};
+  const step = new Set([...a.ints, ...b.ints]);
+
+  for (const key of new Set([...Object.keys(a.val), ...Object.keys(b.val)])) {
+    const av = a.val[key];
+    const bv = b.val[key];
+
+    if (av === undefined) {
+      out[key] = bv ?? 0;
+    } else if (bv === undefined) {
+      out[key] = av;
+    } else if (step.has(key)) {
+      out[key] = t < 0.5 ? av : bv;
+    } else {
+      out[key] = mix(av, bv, t);
+    }
+  }
+
+  return { val: out, ints: [...step] };
+}
