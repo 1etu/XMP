d585e5a9e3c849b023c83108a8083ac864c087d2
ege türker <183089834+1etu@users.noreply.github.com>
2024-01-31 09:17:30 +0300

    Declare Hdr in the particle view

--- packages/qgl/src/preset/view.ts
+++ packages/qgl/src/preset/view.ts
@@ -11,3 +11,23 @@ export interface Bg {
   readonly dayspread: number;
   readonly nightWhitBias: number;
 }
+
+export interface Hdr {
+  readonly enabled: number;
+  readonly exposure: number;
+  readonly whiteLevel: number;
+  readonly glareLevel: number;
+  readonly glareThresh: number;
+  readonly gaussianRadR: number;
+  readonly gaussianRadG: number;
+  readonly gaussianRadB: number;
+  readonly glareSumPow: number;
+  readonly texSize: number;
+  readonly texMaxMip: number;
+  readonly glare: number;
+  readonly glareOnly: number;
+  readonly tonebefore: number;
+  readonly blur: number;
+  readonly dither: number;
+  readonly gamma: number;
+}
