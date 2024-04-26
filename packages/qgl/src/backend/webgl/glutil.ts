d97bf347725824aa7be4f7e08b64b9c02e1501d0
ege türker <183089834+1etu@users.noreply.github.com>
2024-04-26 11:42:03 +0300

    Declare Target in the GL helpers

--- packages/qgl/src/backend/webgl/glutil.ts
+++ packages/qgl/src/backend/webgl/glutil.ts
@@ -2,3 +2,10 @@ import { QglInitError } from "../../frame.js";
 
 export type Gl = WebGL2RenderingContext;
 export type Uniforms = Readonly<Record<string, WebGLUniformLocation | null>>;
+
+export interface Target {
+  readonly tex: WebGLTexture;
+  readonly fbo: WebGLFramebuffer;
+  readonly wid: number;
+  readonly hgt: number;
+}
