35135aa56feeffe46eb47deb2309b996847962ae
ege türker <183089834+1etu@users.noreply.github.com>
2024-07-01 09:52:36 +0300

    Add the WebGL shaders

---
+++ packages/qgl/src/backend/webgl/shaders.ts
@@ -0,0 +1,7 @@
+export * from "./shaders/dither.js";
+export * from "./shaders/bg.js";
+export * from "./shaders/comp.js";
+export * from "./shaders/glare.js";
+export * from "./shaders/part.js";
+export * from "./shaders/quad.js";
+export * from "./shaders/wave.js";
