e9a6601c320f51b50efd73b5a9b0178c8a569b3a
ege türker <183089834+1etu@users.noreply.github.com>
2024-10-21 13:18:15 +0300

    Add the WebGPU front end

---
+++ packages/qgl/src/backend/webgpu/index.ts
@@ -0,0 +1,18 @@
+import { QglInitError, budgetOf } from "../../frame.js";
+import type { QglBackend, QglFrame, Viewport } from "../../frame.js";
+import { STEX_H, STEX_W } from "../../lines/spline.js";
+import {
+  EXPOSURE_SAMPLES,
+  glareLayout,
+  writeExposure,
+  writeGaussian,
+  writeGlareWeights,
+  writeParticleBlur,
+  writeHalfPalette,
+} from "../../hdr.js";
+import { LUT_W, PAL_H, PAL_W } from "../../palette.js";
+import { design, measured } from "@vsh/resource";
+import { MAX_PARTICLES, PARTICLE_FLOATS, Particles } from "../../particles.js";
+import { BG_WGSL, PART_WGSL, POST_WGSL, WAVE_WGSL } from "./shaders.js";
+
+const ROWS = measured(128);
