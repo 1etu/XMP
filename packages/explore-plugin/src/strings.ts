d963ff901d8c13642ef38d3310cc9269319be7ea
ege türker <183089834+1etu@users.noreply.github.com>
2025-02-03 14:56:36 +0300

    Declare Strings in string resolution

--- packages/explore-plugin/src/strings.ts
+++ packages/explore-plugin/src/strings.ts
@@ -1,3 +1,9 @@
 export interface StringsFile {
   readonly text: Readonly<Record<string, string>>;
 }
+
+export interface Strings {
+  of(key: string): string;
+  has(key: string): boolean;
+  readonly missing: readonly string[];
+}
