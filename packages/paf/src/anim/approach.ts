c2b582acd3bcae1591a2e57838c20a6f32b2f193
ege türker <183089834+1etu@users.noreply.github.com>
2025-04-11 17:29:24 +0300

    Add MS_PER_S in the approach curve

--- packages/paf/src/anim/approach.ts
+++ packages/paf/src/anim/approach.ts
@@ -6,3 +6,4 @@ const BOOST = verified(Math.fround(1.000001));
 const SHORT = [1, 0.96153849, 0.78125, 0.63694263, 0.52910054].map(verified);
 const SLOPE = verified(Math.fround(0.33333299));
 const INTERCEPT = verified(Math.fround(2.2));
+const MS_PER_S = verified(Math.fround(0.001));
