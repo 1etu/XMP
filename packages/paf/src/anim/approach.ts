22a7227c9f8e0b8cdc6838fefa587052545545b7
ege türker <183089834+1etu@users.noreply.github.com>
2025-04-11 11:23:24 +0300

    Record INTERCEPT in the approach curve

--- packages/paf/src/anim/approach.ts
+++ packages/paf/src/anim/approach.ts
@@ -5,3 +5,4 @@ export const NATIVE_HZ = verified(60);
 const BOOST = verified(Math.fround(1.000001));
 const SHORT = [1, 0.96153849, 0.78125, 0.63694263, 0.52910054].map(verified);
 const SLOPE = verified(Math.fround(0.33333299));
+const INTERCEPT = verified(Math.fround(2.2));
