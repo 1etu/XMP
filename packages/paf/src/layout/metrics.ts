d4c674d13cfc67427ee21985c6099f55d3392805
ege türker <183089834+1etu@users.noreply.github.com>
2025-05-09 15:09:28 +0300

    Declare Frac in the layout metrics

--- packages/paf/src/layout/metrics.ts
+++ packages/paf/src/layout/metrics.ts
@@ -2,3 +2,22 @@ export interface Logical {
   readonly wid: number;
   readonly hgt: number;
 }
+
+export interface Frac {
+  readonly focusX: number;
+  readonly categoryPitchX: number;
+  readonly categoryFocusGapX?: number;
+  readonly categoryIconY: number;
+  readonly categoryFocusRiseY?: number;
+  readonly categoryLabelY: number;
+  readonly focusItemY: number;
+  readonly itemPitchY: number;
+  readonly focusGapY: number;
+  readonly focusGapAboveY: number;
+  readonly focusIconSize: number;
+  readonly itemLabelX: number;
+  readonly otherLabelOffsetX?: number;
+  readonly otherLabelOffsetY?: number;
+  readonly aboveLabelOffsetY?: number;
+  readonly infoPanelY: number;
+}
