122ff3b80cfb4dcfed545ae09010db46c546a00a
ege türker <183089834+1etu@users.noreply.github.com>
2025-05-27 11:20:58 +0300

    Add the layout slots

---
+++ packages/paf/src/layout/slot.ts
@@ -0,0 +1,9 @@
+export interface Slot {
+  readonly id: string;
+  readonly icon: number;
+  readonly x: number;
+  readonly y: number;
+  readonly size: number;
+  readonly alpha: number;
+  readonly focused: boolean;
+}
