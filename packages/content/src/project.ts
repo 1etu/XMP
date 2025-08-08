9ceacc6cd6cecf759749d6dc17d4f3c10dfbbcd1
ege türker <183089834+1etu@users.noreply.github.com>
2025-08-08 11:38:16 +0300

    Add the project model

---
+++ packages/content/src/project.ts
@@ -0,0 +1,14 @@
+import { catalog } from "./catalog.js";
+
+export interface Project {
+  readonly id: string;
+  readonly title: string;
+  readonly description: string;
+  readonly language: string;
+  readonly source: string;
+  readonly website: string;
+  readonly icon: string;
+  readonly images: readonly { readonly src: string; readonly title: string }[];
+  readonly video: string;
+  readonly paragraphs: readonly string[];
+}
