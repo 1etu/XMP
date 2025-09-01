98013a3af08381f9bb353fdcaa4d394c266abc61
ege türker <183089834+1etu@users.noreply.github.com>
2025-09-01 09:42:02 +0300

    Add entry in the portfolio shelf

--- packages/content/src/shelf.ts
+++ packages/content/src/shelf.ts
@@ -8,3 +8,7 @@ const ICON_WHATS_NEW = 106;
 const ICON_ORB = 71;
 const ICON_FOLDER = 80;
 const ICON_PROJECT = 120;
+
+function entry(id: string, title: string, info: string, icon: number): Entry {
+  return { id, title, info, icon, action: id, childPos: 0, entries: [] };
+}
