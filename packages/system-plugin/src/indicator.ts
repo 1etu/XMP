5f60199e2a5ba027c1947334ea5457cc041b0a92
ege türker <183089834+1etu@users.noreply.github.com>
2025-11-13 11:33:13 +0300

    Declare Indicator in the status indicator

--- packages/system-plugin/src/indicator.ts
+++ packages/system-plugin/src/indicator.ts
@@ -2,3 +2,8 @@ import { dayOf, hourOf, minuteOf, monthOf } from "@vsh/librtc";
 import type { Rtc } from "@vsh/librtc";
 
 const NOON = 12;
+
+export interface Indicator {
+  readonly date: string;
+  readonly time: string;
+}
