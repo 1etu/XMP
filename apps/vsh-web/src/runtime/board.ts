6c9c93a2610abb1dd33702178fe37de4cd4a7387
ege türker <183089834+1etu@users.noreply.github.com>
2026-03-25 11:23:24 +0300

    Record BOARD_TITLE_DELAY_MS in the information board

--- apps/vsh-web/src/runtime/board.ts
+++ apps/vsh-web/src/runtime/board.ts
@@ -4,3 +4,4 @@ import { verified } from "@vsh/qgl";
 
 export const BOARD_EXPAND_MS = verified((8 * 1000) / 60);
 export const BOARD_SCROLL_SPEED = verified(120);
+export const BOARD_TITLE_DELAY_MS = verified(1000);
